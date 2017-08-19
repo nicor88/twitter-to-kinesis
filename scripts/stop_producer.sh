28ebc2d7273990f18b125b50ce734381e700a75b
nicor88 <ncorda@babbel.com>
2017-08-20 00:56:52 +0200

    added again path with conda

--- scripts/stop_producer.sh
+++ scripts/stop_producer.sh
@@ -2,6 +2,7 @@
 
 export AWS_DEFAULT_REGION="eu-west-1"
 export PYTHONPATH=/opt/twitter-to-kinesis
+export PATH=/home/ec2-user/miniconda/bin:$PATH
 
 # create or update conda env
 conda env update -n twitter-to-kinesis -f /opt/twitter-to-kinesis/conda-dev-env.yml
