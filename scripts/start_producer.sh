902b082dcf60fa7c550f52f97abbcdbbb01b8ee2
nicor88 <ncorda@babbel.com>
2017-08-20 00:16:23 +0200

    changed location for now

--- scripts/start_producer.sh
+++ scripts/start_producer.sh
@@ -2,7 +2,6 @@
 
 export AWS_DEFAULT_REGION="eu-west-1"
 export PYTHONPATH=/opt/twitter-to-kinesis
-export PATH=/home/ec2-user/miniconda/bin:$PATH
 
 # create or update conda env
 conda env update -n twitter-to-kinesis -f /opt/twitter-to-kinesis/conda-dev-env.yml
@@ -13,4 +12,4 @@ cp /root/configs.yml /opt/twitter-to-kinesis/settings
 cd /opt/twitter-to-kinesis/
 
 # start producer
-# python /opt/twitter-to-kinesis/sender_daemon.py --pid_file /tmp/twitter_sender.pid start &>/dev/null &
+python /opt/twitter-to-kinesis/sender_daemon.py --pid_file /tmp/twitter_sender.pid start &>/dev/null &
