2cd174019d915997e645f4c8c2d3d5697011f6e1
nicor88 <ncorda@babbel.com>
2017-08-19 21:34:55 +0200

    added code deploy

---
+++ codedeploy/stop_producer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION="eu-west-1"
+export PYTHONPATH=/opt/twitter-to-kinesis
+export PATH=/home/ec2-user/miniconda/bin:$PATH
+
+# create or update conda env
+conda env update -n twitter-to-kinesis -f /opt/twitter-to-kinesis/conda-dev-env.yml
+
+cd /opt/twitter-to-kinesis/
+python /opt/twitter-to-kinesis/sender_daemon.py --pid_file /tmp/twitter_sender.pid stop &>/dev/null &
